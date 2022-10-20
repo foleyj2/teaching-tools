29264ddb4ed53ea0c4bcd0c254dcc3953eb441dc
Joseph Timothy Foley <foley@ru.is>
2022-10-20 23:54:27 +0000

    Logging works in the Accountant

--- AssignmentAccountant.py
+++ AssignmentAccountant.py
@@ -11,7 +11,8 @@ from datetime import datetime
 import re
 
 class AssignmentAccountant():
-    def __init__(self,infd):
+    Ledger = {}
+    def __init__(self, infd, logger):
         for line in infd:
             maybelines = line.strip().rstrip()
             for line in maybelines.splitlines():# in case it is a multiline comment
@@ -22,7 +23,12 @@ class AssignmentAccountant():
                 detect_grade_re = re.compile(r'(^[A-Z]{2,}\d?\!?)(\(\S+\))?\:?(.*)')
                 checkedline = detect_grade_re.match(line)
                 if checkedline:
-                    print(f"CODE: {checkedline.group(1)} OPTVAL: {checkedline.group(2)} COMMENT: {checkedline.group(3)}")
+                    code, optval, comment = checkedline.groups()
+                    logger.warning(f"CODE: {code} OPTVAL: {optval} COMMENT: {comment}")
+                    self.parse_values(code, optval, comment)
+    def parse_values(self, code, optval, comment):
+        """Take comment and turn into operations on ledger"""
+        pass
 
     def dump_values(self,outfd):
         '''Given a file descriptor, iterate line by line and parse comments'''
@@ -35,19 +41,37 @@ def main():
     parser = argparse.ArgumentParser(
         description="Process Comments into Assignment values.")
     parser.add_argument('filepaths', nargs=argparse.REMAINDER)
+    ## TODO:  Fix verbosity based upon another argument and logging
     parser.add_argument('--log', default="INFO",
-        help='Log level:  Number or DEBUG, INFO, WARNING, ERROR')
+        help='Console log level:  Number or DEBUG, INFO, WARNING, ERROR')
     parser.add_argument('--ext', default=".act",
                         help='Extension for output')
 
     args = parser.parse_args()
+    ## Set up logging
     numeric_level = getattr(logging, args.log.upper(), None)
     if not isinstance(numeric_level, int):
         raise ValueError('Invalid log level: %s' % args.log)
-    datestring = 'AssignmentAccountant-{:%Y%m%d-%H%M%S}.log'.format(datetime.now())
-    logging.basicConfig(format='%(message)s',
-                        filename=datestring, level=numeric_level)
-    logging.info("Creating AsignmentAccountant log file %s", datestring)
+    print(f"Log level:  {numeric_level}")
+    logger = logging.getLogger("app")
+    logger.setLevel(logging.INFO)
+    # log everything to file
+    logpath = 'AssignmentAccountant-{:%Y%m%d-%H%M%S}.log'.format(datetime.now())
+    fh = logging.FileHandler(logpath)
+    fh.setLevel(logging.DEBUG)
+    # log to console
+    ch = logging.StreamHandler()
+    ch.setLevel(numeric_level)
+    # create formatter and add to handlers
+    consoleformatter = logging.Formatter('%(message)s')
+    ch.setFormatter(consoleformatter)
+    spamformatter = logging.Formatter('%(asctime)s %(name)s[%(levelname)s] %(message)s')
+    fh.setFormatter(spamformatter)
+    # add the handlers to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+    logger.info("Creating AssignmentAccountant log file %s", logpath)
 
     for filepath in args.filepaths:
       # filename pre-processing for output
@@ -64,7 +88,7 @@ def main():
           input = open(inpath)
       
       with open(outpath, "w") as outfd:
-          AA = AssignmentAccountant(input)
+          AA = AssignmentAccountant(input,logger)
           AA.dump_values(outfd)
 if __name__ == "__main__":
   main()
